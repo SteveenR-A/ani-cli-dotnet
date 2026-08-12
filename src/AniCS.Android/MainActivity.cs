592b1ec64f0a0bd72e87df233ef129d5b1e7133f
SteveenR-A <202854165+SteveenR-A@users.noreply.github.com>
2026-08-12 16:58:01 -0600

    Fix: AvaloniaMainActivity is non-generic in Avalonia 12

--- src/AniCS.Android/MainActivity.cs
+++ src/AniCS.Android/MainActivity.cs
@@ -20,6 +20,6 @@ namespace AniCS.Android;
         global::Android.Content.PM.ConfigChanges.UiMode |
         global::Android.Content.PM.ConfigChanges.Keyboard |
         global::Android.Content.PM.ConfigChanges.KeyboardHidden)]
-public class MainActivity : AvaloniaMainActivity<App>
+public class MainActivity : AvaloniaMainActivity
 {
 }
